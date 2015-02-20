bc7e74ed4200dc3ce281cc34a91eb02ec820f9fa
Kiran Kumar <kamruthaluri@innominds.com>
2015-02-21 00:50:58 +0530

    added bredenhem's line algo - rosetta code

--- js/main.js
+++ js/main.js
@@ -3,8 +3,7 @@ $(function(){
 	try {
 	  console.log(d3, "D3 is Intialiased");
 	}
-	catch(err)
-        {
+	catch(err) {
        throw "D3 is not initialised";
 	}
 
@@ -13,15 +12,15 @@ $(function(){
 	 */
 
 	var container = {
-	    width:400,
-	    height:400
+	    width:800,
+	    height:300
 	};
 
 	/*
 	 	Canvas for the Plane & Projection
 	 */
 
-	var canvas = d3.select('body').append('svg').attr({
+	var canvas = d3.select('#line-naive-algo').attr({
 	   "width":container.width,
 	   "height":container.height
 	});
@@ -43,7 +42,10 @@ $(function(){
 	 * Naive Line Drawing Algorithm 
 	 */
 
-	 // Give Two Points
+	 /**
+	  * {param} x-point, y-point
+	  * Constructor for the point
+	  */
 
 	 function Point(x,y){
 	 	this.x = x;
@@ -55,20 +57,25 @@ $(function(){
 
 	 var _dx = ptTwo.x - ptOne.x,
 	 	 _dy = ptTwo.y - ptOne.y,
-	 	 x,y;
+	 	 x,y,
+	 	 _tempArray = [],
+	 	 _pathCoords;
 
 	  for(x = ptOne.x; x <= ptTwo.x; x++){
 	  	y = ptOne.y + _dy * ( (x - ptOne.x) / _dy);
-	  	plot(x,y);
+	  	_pathCoords = x+" "+y;
+	  	_tempArray.push(_pathCoords);
 	  }
 
 	  /*
 		Point for the Line Drawing Alogorithm	
 	  */
 
-	  function plot(x,y){
-	  	canvas.append('circle').attr({"cx":x+2,"cy":y+2,"r":2});
+	  function render(){
+	  	canvas.append('path').attr({"d":"M"+_tempArray.join(" ") +"Z", "stroke":"red", "stroke-width":5});
 	  }
+
+	  render();
 
 	  /*
 	  	DDA - Digital Digital Analyzer
@@ -76,5 +83,50 @@ $(function(){
 	  
 	   // Todo - Need to be done
 
+	  /*
+	  	Bresenham Algorithm
+	   */
+	  
+	  var bresenhamCanvas = d3.select('#line-bresenhem-algo')
+	  							.attr({
+	  								"width":800,
+	  								"height":800
+	  							}); 
 
+	  // Regular way of doing algorithm
+	  function bline(x0, y0, x1, y1) {
+
+	  	/*
+		  // bresenhamCanvas.append('rect').attr({'x':10,'y':10, 'width':20, 'height':20,'stroke':'red', 'stroke-width':5, 'fill':'red' });
+		  var dx = x1-x0, dy = y1-y0 , tdx = 2*dx , tdy = 2*dy, pk = tdy-tdx, po = tdy - dx;
+		  console.log(x0, y0, x1, y1);
+		  console.log('dx:',dx,'dy:',dy, 'm:',dy/dx , 'tdx:', tdx, 'tdy:',tdy, 'pk:', pk, 'po:',po);
+		  setPixel(x0,y0);
+	
+		  Count it and do things accordingly with decision parameter
+	
+		*/
+	    
+	    // Rosetta Code Algorithm - Need to do a video on this with the help of unacademy tutorials
+	    var dx = Math.abs(x1-x0), sx = x0 < x1 ? 1 : -1;
+	    var dy = Math.abs(y1-y0), sy = y0 < y1 ? 1 : -1;
+	    var err = (dx>dy ? dx : -dy)/2;
+
+	    while(true){
+	    	setPixel(x0,y0);
+	    	if( x0 === x1 && y0 === y1 ) break;
+	    	var e2 = err;
+	    	if( e2 < dy ){ err +=dx; y0 += sy; }
+	        if( e2 > -dx){ err -=dy; x0 += sx; }
+	    }	
+
+	  }
+
+	  function setPixel(x,y){
+	  	   bresenhamCanvas.append('rect').attr({'x':x*10,'y':y*10, 'width':10, 'height':10,'stroke':'red', 'stroke-width':5, 'fill':'red' });
+	  }
+
+	  // 10, 10 - 20, 20
+
+	  bline(1,1,10,5);
 });
